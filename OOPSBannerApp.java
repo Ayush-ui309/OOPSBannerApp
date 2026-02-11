fe6d67d2ec578ac18c50cd5d5b3d18a08ab330a6
Ayush-ui309 <ayushrktsingh2006@gnmail.com>
2026-02-12 01:44:42 +0530

    extend uc1 by displaying 'OOPS' in large banner form spaces'*' characters.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,12 @@
-/**
- * OOPSBannerApp - UC1
- * This class demonstrates a simple Java application that display the object-oriented programming system OOPS acronym to the  console.
- * @author Ayush
- * @version 1.0
- */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "    *****");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + "**    ** " + " " + "  **     ");
+        System.out.println("**     **" + " " + "**     **" + " " + "**     **" + " " + "**       ");
+        System.out.println("**     **" + " " + "**     **" + " " + "******   " + " " + "  *****  ");
+        System.out.println("**     **" + " " + "**     **" + " " + "**       " + " " + "       **");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " " + " **    **");
+        System.out.println("   ***   " + " " + "   ***   " + " " + "**       " + " " + "   ***** ");
     }
-}
+}    
+    
